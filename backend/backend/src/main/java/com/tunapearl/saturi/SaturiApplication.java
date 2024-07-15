b3a304bff59ec5f8665f876c29ea6445831db4e5
이진주 <dlwlswn3539@naver.com>
2024-07-15 15:47:50 +0900

    Initial commit

---
+++ backend/backend/src/main/java/com/tunapearl/saturi/SaturiApplication.java
@@ -0,0 +1,13 @@
+package com.tunapearl.saturi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaturiApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/tunapearl/saturi/SaturiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunapearl.saturi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaturiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
