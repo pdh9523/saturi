03d825bb1e5e17698d3a41ca6a07b2c6b2ba6ff0
허동원 <heo_dongwon@naver.com>
2024-07-20 23:44:11 +0900

    [BE]chore: 소셜 로그인용 DTO 생성

---
+++ backend/src/main/java/com/tunapearl/saturi/dto/SocialUserResponse.java
@@ -0,0 +1,15 @@
+package com.tunapearl.saturi.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class SocialUserResponse {
+    private Long userId;
+    private String email;
+    private String gender;
+    private String ageRange;
+}
