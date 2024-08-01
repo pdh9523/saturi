2e89c668f8c30fafb1a6e89b71bb9af525af62e7
허태훈 <lazenca0126@gmail.com>
2024-08-01 17:04:06 +0900

    임시 merge2(허태훈)

--- frontend/src/app/(home)/main/middlepart.tsx
+++ frontend/src/app/(home)/main/middlepart.tsx
@@ -1,21 +1,4 @@
 interface MiddlePartProps {
-<<<<<<< HEAD
-  middlePosition: any;
-  mainPageIndicator: any;
-  selectedRegion: any;
-  style: any;
-}
-
-export default function MiddlePart({
-  middlePosition,
-  mainPageIndicator,
-  selectedRegion,
-  style,
-}: MiddlePartProps) {
-  return (
-    <div
-      className="middlepart"
-=======
   middlePosition: number;
   mainPageIndicator: string;
   // selectedRegion?: string;
@@ -25,37 +8,10 @@ export default function MiddlePart({ middlePosition, mainPageIndicator }: Middle
   return (
     <div 
       className="middlepart" 
->>>>>>> feat/FE/mainTmp
       style={{
         left: (() => {
           if (middlePosition === 0) {
             return "-110%";
-<<<<<<< HEAD
-          }
-          if (middlePosition === 1) {
-            return "0%";
-          }
-          if (middlePosition === 2) {
-            return "110%";
-          }
-          return undefined;
-        })(),
-      }}
-    >
-      <h1
-        className="blink"
-        style={{
-          position: "absolute",
-          top: "75%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          fontSize: 25,
-        }}
-      >
-        {" "}
-        {mainPageIndicator}{" "}
-      </h1>
-=======
           } if (middlePosition === 1) {
             return "0%";
           } if (middlePosition === 2) {
@@ -72,7 +28,6 @@ export default function MiddlePart({ middlePosition, mainPageIndicator }: Middle
           transform: 'translate(-50%, -50%)',
           fontSize: 25,
         }}> {mainPageIndicator} </h1>
->>>>>>> feat/FE/mainTmp
     </div>
   );
 }
