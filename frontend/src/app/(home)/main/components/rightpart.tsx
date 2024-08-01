466d17bb9ebbb65ed4d04a8a8079a669e3da4a2d
허태훈 <lazenca0126@gmail.com>
2024-08-01 22:14:55 +0900

    [FE]feat: 메인 파트 commit

---
+++ frontend/src/app/(home)/main/components/rightpart.tsx
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/navigation';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Card from "@mui/material/Card";
+import Image from "next/image"
+import Typography from '@mui/material/Typography';
+
+export default function RightPart() {
+  const router = useRouter();
+
+  function GameStartButton() {
+    router.push("/game");
+  }
+
+  return (
+    <div className="rightpart" style={{ height: '100vh' }}>
+      <Box sx={{ display: 'grid', placeItems: 'center', height: '85vh' }}>
+        <Card sx={{ width: "65vw", height:"75vh", border: '2px solid black', borderRadius: 5, top:"-50%", backgroundColor: "whitesmoke" }} />
+      </Box>
+
+      <Box sx={{ position: 'absolute', margin: "25px", top: "7%", right: "20%", width: "25vw", display:"flex", flexDirection: "column",  alignItems: "center" }}>
+        <Typography variant="h1" sx={{ fontSize: 39, fontWeight: "bold" }}>게임 페이지</Typography>
+        <br />
+        <Box sx={{  }}>
+          <Card sx={{ width:"15vw", height: "39vh", display: 'grid', placeItems: 'center' }}>
+            <Image src="/MainPage/myLevel.png" alt="button food" width={200} height={200} />
+          </Card>                  
+        </Box>
+        <Button variant="contained" onClick={GameStartButton} sx={{ marginTop: "20px", width: "15vw", height: "5vh", fontSize: "25px"  }}>게임 시작</Button>
+      </Box>
+    </div>
+  );
+}
