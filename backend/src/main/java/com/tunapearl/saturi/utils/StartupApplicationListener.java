b2a68f551c1b591bff831c73dc42f59e580f3fd3
hajae <gkrhf@naver.com>
2024-08-02 09:31:50 +0900

    [BE]chore: 레슨 그룹 샘플 데이터 추가, 파일 저장 경로 변경

--- backend/src/main/java/com/tunapearl/saturi/utils/StartupApplicationListener.java
+++ backend/src/main/java/com/tunapearl/saturi/utils/StartupApplicationListener.java
@@ -2,9 +2,12 @@ package com.tunapearl.saturi.utils;
 
 import com.tunapearl.saturi.domain.LocationEntity;
 import com.tunapearl.saturi.domain.lesson.LessonCategoryEntity;
+import com.tunapearl.saturi.domain.lesson.LessonGroupEntity;
 import com.tunapearl.saturi.dto.user.UserRegisterRequestDTO;
+import com.tunapearl.saturi.repository.LocationRepository;
 import com.tunapearl.saturi.service.game.GameService;
 import com.tunapearl.saturi.service.lesson.AdminLessonService;
+import com.tunapearl.saturi.service.lesson.LessonService;
 import com.tunapearl.saturi.service.user.BirdService;
 import com.tunapearl.saturi.service.user.LocationService;
 import com.tunapearl.saturi.service.user.UserService;
@@ -14,6 +17,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 실행 시 샘플 데이터 추가를 위한 class
@@ -28,6 +33,7 @@ public class StartupApplicationListener {
     private final UserService userService;
     private final GameService gameService;
     private final AdminLessonService adminLessonService;
+    private final LessonService lessonService;
 
     private static final String[] LOCATION_NAMES = {"default", "gyungsang", "gyunggi", "gangwon", "chungcheong", "jeonra", "jeju"};
     private static final String[] BIRD_NAMES = {"agent", "banker", "clown", "sailor", "deckhand", "evil_king", "cavalry", "cute_killer",
@@ -40,19 +46,25 @@ public class StartupApplicationListener {
     private static final String[] Tips = {"타자가 빠르면 유리합니다.", "손가락의 유연성을 높이기 위한 운동을 하세요.", "화면을 주의 깊게 살펴보며 빠르게 대응하세요.",
             "손가락을 워밍업하고 시작하세요.", "빠른 판단력이 중요합니다."};
     private static final String[] LESSON_CATEGORIES = {"일상", "드라마 대사", "영화 대사", "밈"};
-    
+    private static final String[] LESSON_GROUP_NAME = {"첫번째 퍼즐", "두번째 퍼즐", "세번째 퍼즐", "네번째 퍼즐", "다섯번째 퍼즐"};
+
+
+
     //TODO 레슨 샘플 데이터 등록 필요
-    
+
+
+
     //TODO 퀴즈 샘플 데이터 등록 필요
 
     @EventListener
     @Transactional
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        createLocation();
+        createLocation(); // 지역 샘플 추가(
         createBird();
         createUser();
         createTip();
         createLessonCategory();
+        createLessonGroup();
     }
 
     private void createLocation() {
@@ -101,6 +113,22 @@ public class StartupApplicationListener {
         for (int i = 0; i < LESSON_CATEGORIES.length; i++) {
             LessonCategoryEntity lessonCategory = adminLessonService.createLessonCategory(LESSON_CATEGORIES[i]);
             log.info("create lessonCategory sample {}", lessonCategory);
+        }
+    }
+
+    //TODO 레슨 그룹 샘플 데이터 등록
+    private void createLessonGroup() {
+        List<LocationEntity> locations = locationService.findAll();
+        List<LessonCategoryEntity> lessonCategories = lessonService.findAllLessonCategory();
+        for (int i = 1; i < 3; i++) {
+            LocationEntity location = locations.get(i);
+            for (int j = 0; j < 4; j++) {
+                LessonCategoryEntity lessonCategory = lessonCategories.get(j);
+                for (int k = 0; k < LESSON_GROUP_NAME.length; k++) {
+                    String lessonGroupName = location.getName() + " " + lessonCategory.getName() + " " + LESSON_GROUP_NAME[k];
+                    adminLessonService.createLessonGroup(location, lessonCategory, lessonGroupName);
+                }
+            }
         }
     }
 
