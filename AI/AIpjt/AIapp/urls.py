937dd7377200ddb23333a8dfe31b67309c8becbf
김도환 <rlaehghkr@gmail.com>
2024-08-04 00:03:46 +0900

    [AI] feat: AI 기능 구현

--- AI/AIpjt/AIapp/urls.py
+++ AI/AIpjt/AIapp/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'audioapp'
 
 urlpatterns = [
-    path('', views.audio_list, name='audio_list'),
-    path('upload/', views.upload_audio, name='upload_audio'),
     path('analyze/', views.analyze_audio, name='analyze_audio'),
 ]
--- AI/AIpjt/AIapp/views.py
+++ AI/AIpjt/AIapp/views.py
@@ -1,181 +1,192 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+import io
+import json
+import tempfile
+import logging
+from decimal import Decimal
+from google.cloud import storage
+import requests
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.conf import settings
+from django.http import JsonResponse
+from .serializers import CommingDataSerializer
+from difflib import SequenceMatcher
 import parselmouth
 from parselmouth.praat import call
-# Todo: render, redirect 모두 rest_framework 로 바꾸기
-
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
-# from rest_framework import status
-from django.shortcuts import render, redirect
-from django.conf import settings
-from .forms import AudioFileForm
-from .models import AudioFile
-from difflib import SequenceMatcher
-import wave
-import json
-import requests
-import logging
+import numpy as np
 
 # 로깅 설정
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-class ClovaSpeechClient:
-    # Clova Speech invoke URL
-    invoke_url = settings.INVOKE_URL
-    # Clova Speech secret key
-    secret = settings.SECRET
+def load_wav_file_from_gcs(file_name, bucket_name='saturi'):
+    """Google Cloud Storage에서 WAV 파일을 메모리에 로드합니다."""
+    try:
+        # Google Cloud Storage 클라이언트 초기화
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(file_name)
 
-    def req_upload(self, file, completion, callback=None, userdata=None, forbiddens=None, boostings=None,
-                   wordAlignment=True, fullText=True, diarization=None, sed=None):
+        # 파일 존재 여부 확인
+        if not blob.exists():
+            logger.error(f"File '{file_name}' does not exist in bucket '{bucket_name}'.")
+            return None
+
+        # 파일을 메모리로 다운로드
+        file_data = io.BytesIO()
+        blob.download_to_file(file_data)
+        file_data.seek(0)  # 파일 포인터를 되감기
+        logger.info(f"File '{file_name}' successfully loaded into memory.")
+        return file_data
+
+    except Exception as e:
+        logger.error(f"Error loading file '{file_name}' from Google Cloud Storage: {e}")
+        raise
+
+def extract_pitch(audio_data):
+    """메모리에 있는 WAV 오디오 데이터에서 피치 값을 추출합니다."""
+    try:
+        # 포인터 위치 초기화
+        audio_data.seek(0)
+        # 임시 파일 생성
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_wav_file:
+            temp_wav_file.write(audio_data.read())  # BytesIO 객체의 내용을 파일에 씁니다
+            temp_wav_file.flush()  # 내용을 디스크에 저장
+            temp_wav_file.seek(0)  # 파일 포인터를 처음으로 이동
+
+            # 임시 파일 경로를 사용하여 Sound 객체 생성
+            audio = parselmouth.Sound(temp_wav_file.name)
+            pitch = call(audio, "To Pitch", 0.0, 75, 600)  # 75Hz에서 600Hz 범위로 피치 계산
+            pitch_values = pitch.selected_array['frequency']
+            pitch_values = pitch_values[pitch_values != 0]  # 0 값을 제거
+            return pitch_values.tolist()
+    except Exception as e:
+        logger.error(f"Error extracting pitch: {e}")
+        raise
+
+
+def transcribe_audio(audio_data):
+    """오디오 데이터를 Clova Speech API를 사용하여 텍스트로 변환합니다."""
+    client = ClovaSpeechClient()
+    response = client.req_upload(audio_data)
+
+    if response is None:
+        return None
+
+    try:
+        response_json = response.json()
+    except json.JSONDecodeError:
+        logger.error("JSON 응답 디코딩 실패.")
+        return None
+
+    logger.debug(f"API 응답: {response_json}")
+
+    if "segments" not in response_json or not response_json["segments"]:
+        logger.error("응답에 세그먼트가 없습니다.")
+        return None
+
+    transcription = ' '.join(segment['text'] for segment in response_json["segments"])
+    return transcription
+
+class ClovaSpeechClient:
+    invoke_url = settings.CLOVA_SPEECH_INVOKE_URL
+    secret = settings.CLOVA_SPEECH_SECRET
+
+    def req_upload(self, file_data, completion='sync'):
         request_body = {
             'language': 'ko-KR',
             'completion': completion,
-            'callback': callback,
-            'userdata': userdata,
-            'wordAlignment': wordAlignment,
-            'fullText': fullText,
-            'forbiddens': forbiddens,
-            'boostings': boostings,
-            'diarization': diarization,
-            'sed': sed,
+            'wordAlignment': True,
+            'fullText': True
         }
         headers = {
             'Accept': 'application/json;UTF-8',
             'X-CLOVASPEECH-API-KEY': self.secret
         }
         files = {
-            'media': open(file, 'rb'),
+            'media': file_data,
             'params': (None, json.dumps(request_body, ensure_ascii=False).encode('UTF-8'), 'application/json')
         }
+
+        # 추가 디버깅: 파일 크기와 상태 확인
+        file_data.seek(0, io.SEEK_END)
+        logger.debug(f"Uploading file of size: {file_data.tell()} bytes")
+        file_data.seek(0)
+
         try:
             response = requests.post(url=self.invoke_url + '/recognizer/upload', headers=headers, files=files)
             response.raise_for_status()
             return response
         except requests.exceptions.RequestException as e:
-            logger.error(f"Error during Clova Speech API request: {e}")
+            logger.error(f"Clova Speech API 요청 중 오류 발생: {e}")
             return None
+    
 
-# 음성 피치 분석 정보
-def extract_pitch(audio_file):
-    snd = parselmouth.Sound(audio_file)
-    pitch = call(snd, "To Pitch", 0.0, 75, 600)  # 75 Hz to 600 Hz의 범위로 피치를 계산
-    pitch_values = pitch.selected_array['frequency']
-    pitch_values = pitch_values[pitch_values != 0]  # 0인 값 제거
-    return pitch_values
-
-# 오디오 파일의 샘플 레이트 측정하는 함수
-def get_sample_rate(audio_file_path):
-    with wave.open(audio_file_path, 'rb') as wav_file:
-        return wav_file.getframerate()
-
-# 오디오 스크립트화 하는 함수
-def transcribe_audio(audio_file_path):
-    try:
-        client = ClovaSpeechClient()
-        res = client.req_upload(file=audio_file_path, completion='sync')
-        
-        if res is None:
-            return None
-
-        res_json = res.json()
-        logger.debug(f"API Response: {res_json}")
-
-        if "segments" not in res_json or not res_json["segments"]:
-            logger.error("No transcription segments found in response.")
-            return None
-
-        res_text = res_json["segments"][0]["text"]
-        return res_text
-    except Exception as e:
-        logger.error(f"Error transcribing audio: {e}")
-        return None
-
-# 오디오를 업로드 받는 함수
-def upload_audio(request):
-    if request.method == 'POST':
-        form = AudioFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('audioapp:audio_list')
-    else:
-        form = AudioFileForm()
-    return render(request, 'audioapp/upload.html', {'form': form})
-
-# 오디오 리스트 반환하는 함수
-def audio_list(request):
-    audios = AudioFile.objects.all()
-    return render(request, 'audioapp/audio_list.html', {'audios': audios})
-
-# 스크립트 유사도 함수
 def script_similarity_ratio(original, transcribed):
+    """두 스크립트 간의 유사도 비율을 계산합니다."""
     if original is None or transcribed is None:
-        logger.error("One of the transcripts is None.")
+        logger.error("스크립트 중 하나가 None입니다.")
         return 0
     return SequenceMatcher(None, original, transcribed).ratio()
 
-# 오디오 비교해서 분석하는 함수
+@api_view(['POST'])
 def analyze_audio(request):
-    if request.method == 'POST':
-        # Todo : api 통신으로 받아올 데이터를 넣어야 합니다.
-        # 그 중에는 유저음성파일 하나, 정답파일path 가 있을 것 
-        media_path = os.path.join(settings.BASE_DIR, 'media', 'audios')
-        voice1 = os.path.join(media_path, 'voice1.wav')
-        voice2 = os.path.join(media_path, 'voice2.wav')
-
-        if not os.path.exists(voice1) or not os.path.exists(voice2):
-            return render(request, 'audioapp/error.html', {'message': 'Required audio files are missing.'})
+    serializer = CommingDataSerializer(data=request.data)
+    if serializer.is_valid():
+        answer_voice_file = serializer.validated_data['answerVoiceFileName']
+        user_voice_file = serializer.validated_data['userVoiceFileName']
 
         try:
+            # Google Cloud Storage에서 파일 데이터를 메모리에 로드
+            answer_voice_data = load_wav_file_from_gcs(answer_voice_file)
+            user_voice_data = load_wav_file_from_gcs(user_voice_file)
+
+            if answer_voice_data is None or user_voice_data is None:
+                return JsonResponse({'error': 'File not found.'}, status=status.HTTP_404_NOT_FOUND)
+
             # 첫 번째 음원 처리
-            pitch_values1 = extract_pitch(voice1)
-            
+            answer_pitch = extract_pitch(answer_voice_data)
+
             # 두 번째 음원 처리
-            pitch_values2 = extract_pitch(voice2)
+            user_pitch = extract_pitch(user_voice_data)
 
             # 크로스 코릴레이션 유사성 계산
             def cross_correlation_similarity(signal_a, signal_b):
+                if len(signal_a) == 0 or len(signal_b) == 0:
+                    logger.warning("피치 시퀀스 중 하나가 비어 있습니다.")
+                    return 0
                 correlation = np.correlate(signal_a, signal_b, mode='full')
                 max_correlation = np.max(correlation)
                 normalized_correlation = max_correlation / (np.linalg.norm(signal_a) * np.linalg.norm(signal_b))
                 similarity_percent = normalized_correlation * 100
                 return similarity_percent
 
-            similarity = cross_correlation_similarity(pitch_values1, pitch_values2)
+            voice_similarity = cross_correlation_similarity(answer_pitch, user_pitch)
 
             # 음원 텍스트 변환
-            transcript1 = transcribe_audio(voice1)
-            transcript2 = transcribe_audio(voice2)
+            transcript1 = transcribe_audio(answer_voice_data)
+            transcript2 = transcribe_audio(user_voice_data)
 
             if transcript1 is None or transcript2 is None:
-                return render(request, 'audioapp/error.html', {'message': 'Transcription failed.'})
+                return JsonResponse({'error': 'Transcription failed.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
             # 텍스트 유사도
             script_similarity = script_similarity_ratio(transcript1, transcript2) * 100
 
-            # 결과를 문자열로 저장
-            analysis_result = f'Voice Similarity Percent: {similarity}%\n'
-            analysis_result += f'Script Similarity Percent: {script_similarity}%\n'
-            analysis_result += f'Transcription of Audio 1: {transcript1}\n'
-            analysis_result += f'Transcription of Audio 2: {transcript2}\n'
+            # 데이터 생성
+            sending_data = {
+                'voiceSimilarity': float(Decimal(str(voice_similarity))),
+                'scriptSimilarity': float(Decimal(str(script_similarity))),
+                'answerVoicePitch': {'pitches': answer_pitch},
+                'userVoicePitch': {'pitches': user_pitch},
+                'userScript': transcript2
+            }
 
-            # 피치 시각화
-            plt.figure(figsize=(10, 5))
-            plt.plot(pitch_values1, 'r', label='Audio 1', linewidth=2.5)
-            plt.plot(pitch_values2, 'b', label='Audio 2', linewidth=2.5)
-            plt.title('Pitch Contour Comparison', fontsize=20, weight='bold')
-            plt.xlabel('Time', fontsize=15)
-            plt.ylabel('Frequency (Hz)', fontsize=15)
-            plt.legend(title='Legend', title_fontsize='13', fontsize='12')
-            plt.grid(True, linestyle='--', alpha=0.6)
-            plot_file = os.path.join(media_path, 'pitch_comparison.png')
-            plt.savefig(plot_file)
-            plt.close()
+            # JSON으로 반환
+            return JsonResponse(sending_data, status=status.HTTP_200_OK)
+
         except Exception as e:
-            logger.error(f"Error analyzing audio: {e}")
-            return render(request, 'audioapp/error.html', {'message': str(e)})
-
-        return render(request, 'audioapp/analysis_result.html', {'output': analysis_result, 'plot_file': 'audios/pitch_comparison.png'})
-    return redirect('audioapp:audio_list')
+            logger.error(f"오디오 분석 중 오류 발생: {e}")
+            return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
