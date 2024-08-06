8877f22ade8f84a2025119565e740bb1b2abdd86
이진주 <dlwlswn3539@naver.com>
2024-08-06 09:46:21 +0900

    [BE]feat: 게임 결과 조회 틀 구현

--- backend/src/main/java/com/tunapearl/saturi/service/GameRoomParticipantService.java
+++ backend/src/main/java/com/tunapearl/saturi/service/GameRoomParticipantService.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -18,16 +20,20 @@ public class GameRoomParticipantService {
 
     private final GameRoomParticipantRepository gameRoomParticipantRepository;
 
-    public GameRoomParticipantEntity findById(Long roomId,Long userId) {
+    public GameRoomParticipantEntity findById(Long roomId, Long userId) {
 
-        GameRoomParticipantId id=new GameRoomParticipantId(roomId,userId);
+        GameRoomParticipantId id = new GameRoomParticipantId(roomId, userId);
         return gameRoomParticipantRepository.findParticipantByGameRoomParticipantId(id);
     }
 
 
     @Transactional
-    public void updateParticipant(GameRoomParticipantEntity gameRoomParticipantEntity){
+    public void updateParticipant(GameRoomParticipantEntity gameRoomParticipantEntity) {
 
-        gameRoomParticipantEntity.setCorrectCount(gameRoomParticipantEntity.getCorrectCount()+1);
+        gameRoomParticipantEntity.setCorrectCount(gameRoomParticipantEntity.getCorrectCount() + 1);
+    }
+
+    public List<GameRoomParticipantEntity> findParticipantByGameRoomParticipantId(Long gameRoomParticipantId) {
+        return null;
     }
 }
