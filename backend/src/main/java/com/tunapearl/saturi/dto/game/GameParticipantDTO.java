f9086100690b82197b107b6d58924a1e97f475f7
이진주 <dlwlswn3539@naver.com>
2024-08-08 20:19:32 +0900

    [BE]fix: boolean 필드명 변경 버그 수정

--- backend/src/main/java/com/tunapearl/saturi/dto/game/GameParticipantDTO.java
+++ backend/src/main/java/com/tunapearl/saturi/dto/game/GameParticipantDTO.java
@@ -1,5 +1,6 @@
 package com.tunapearl.saturi.dto.game;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 public class GameParticipantDTO {
     private String nickName;
     private long birdId;
+    @JsonProperty("isExited")
     private boolean isExited;
 }
