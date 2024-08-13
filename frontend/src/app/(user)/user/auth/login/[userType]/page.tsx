2cd651cf906d3b1f50c0a75625c88938fb6559c6
박동현 <jinee9523@gmail.com>
2024-08-13 09:49:42 +0900

    [FE]refactor: 소셜로그인 params 개선

--- frontend/src/app/(user)/user/auth/login/[userType]/page.tsx
+++ frontend/src/app/(user)/user/auth/login/[userType]/page.tsx
@@ -1,28 +1,22 @@
 "use client";
 
 import api from "@/lib/axios";
-import { useState } from "react";
 import { authToken } from "@/utils/authutils";
 import { Backdrop, CircularProgress } from "@mui/material";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function App() {
+export default function App({params: {userType}}: {params: {userType: string}}) {
   const router = useRouter();
-  const pathname = usePathname();
   const params = useSearchParams()
-  const [isLoading] = useState(true);
-  
-  if (pathname&&params) {
-  const code = params.get("code");
-  const userType = pathname
-    .substring(pathname.lastIndexOf("/") + 1)
-    .toUpperCase();
+
+  if (params) {
+    const code = params.get("code");
 
   if (typeof window !== "undefined") {
     api
       .post(`${process.env.NEXT_PUBLIC_BACKURL}/user/auth/login`, {
         code,
-        userType,
+        userType: `${userType.toUpperCase()}`,
       })
       .then(response => {
         sessionStorage.setItem("accessToken", response.data.accessToken);
@@ -37,15 +31,18 @@ export default function App() {
             }
             }
           )
-
         router.push("/");
+        })
+      .catch(err => {
+        alert("오류가 발생했습니다. \n 잠시 후 다시 시도해주세요.")
+        router.push("/")
       });
   }
 }
   return (
     <Backdrop
       sx={{ color: "#fff", zIndex: theme => theme.zIndex.drawer + 1 }}
-      open={isLoading}
+      open
     >
       <CircularProgress color="inherit" />
     </Backdrop>
