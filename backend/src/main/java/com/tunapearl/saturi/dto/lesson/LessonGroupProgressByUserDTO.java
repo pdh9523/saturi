39594219e2ddcc4f57b84f3df55698c49c63fb45
hajae <gkrhf@naver.com>
2024-08-13 14:52:51 +0900

    [BE]fix: 선택된 지역과 유형의 퍼즐의 유저별 정보 조회 버그 수정

--- backend/src/main/java/com/tunapearl/saturi/dto/lesson/LessonGroupProgressByUserDTO.java
+++ backend/src/main/java/com/tunapearl/saturi/dto/lesson/LessonGroupProgressByUserDTO.java
@@ -2,9 +2,11 @@ package com.tunapearl.saturi.dto.lesson;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class LessonGroupProgressByUserDTO {
     private Long lessonGroupId;
     private String lessonGroupName;
